5e9fd3e71d4613f6fda5595445fea7affb6b7615
Nicholas Mason <nmt.mason@googlemail.com>
2018-05-20 11:17:44 +0100

    Load API over HTTPS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   var api =
-    'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp';
+    'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp';
   var twitter = 'https://twitter.com/intent/tweet';
 
   var sendQuoteRequest = (function() {
